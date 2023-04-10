46f5d9a0de13ede57f2086da75293c10d942db3f
Adrian Ciołek <ciolek.adrian@protonmail.com>
2023-04-10 17:36:23 +0200

    changes

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -20,9 +20,80 @@ const Footer = () => {
                                 </div>
                             </div>
                         </div>
+                        <div className="col-lg-3 col-md-6 col-sm-6">
+                            <div className="footer-widget footer1-widget footer1-widget2 mb-40">
+                                <div className="footer-widget-title">
+                                    <h4>Coming Soon</h4>
+                                </div>
+                                 <ul>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-6">
+                            <div className="footer-widget footer1-widget footer1-widget3 mb-40 ">
+                                <div className="footer-widget-title">
+                                    <h4>Coming Soon</h4>
+                                </div>
+                                  <ul>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-6">
+                            <div className="footer-widget footer1-widget footer1-widget4 mb-40 ">
+                                <div className="footer-widget-title">
+                                    <h4>Coming Soon</h4>
+                                </div>
+                                <ul>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                    <li><Link href="/">Coming Soon</Link></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
+            <div className="copyright-area copyright1-area">
+                <div className="container">
+                    <div className="copyright1-inner">
+                        <div className="row align-items-center">
+                            <div className="col-lg-3 col-md-6">
+                                <div className="copyright-text copyright1-text">
+                                    © Styl stworzony przez BDevs, zakodowany przez Qwizi
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="irc-item copyright-support copyright1-support copyright-support-lines">
+                                    <div className="irc-item-icon">
+                                        <i className="flaticon-support"></i>
+                                    </div>
+                                    <div className="irc-item-content">
+                                        <p>Coming Soon</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-5 col-md-12">
+                                <form className="subscribe-form subscribe-form-copyright1">
+                                    <input type="text" placeholder="Enter email" />
+                                    <button type="submit">Subscribe<i className="fas fa-paper-plane"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 };
