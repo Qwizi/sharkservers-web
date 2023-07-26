617bfbcd23aca7a70425c9b5353fefc7178f90a6
Adrian Ciołek <ciolek.adrian@protonmail.com>
2023-07-26 19:43:29 +0200

    Updated activate account form and page

--- src/app/auth/activate-account/success/page.tsx
+++
@@ -1,9 +0,0 @@
-import TitleSection from "@/components/Layout/TitleSection";
-
-export default function SuccessActivationAccountPage() {
-    return (
-        <>
-            Pomyslnie aktywowano konto
-        </>
-    )
-}
--- src/components/Auth/ActivateAccountForm.tsx
+++ src/components/Auth/ActivateAccountForm.tsx
@@ -1,103 +1,48 @@
 'use client';
-import {createRef, useEffect, useRef, useState} from "react";
-import {activate_account} from "@/lib/fetchApi";
-import {redirect, useRouter} from "next/navigation";
-import AlertMessage from "@/components/Elements/AlertMessage";
-import apiClient from "@/lib/api";
-import {toast} from "react-toastify";
+import {useForm} from "react-hook-form";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+import {SharkServersClient as shark_api} from "sharkservers-sdk";
 
 const ActivateAccountForm = () => {
-    const codeDefault = [
-        "", // code 1
-        "", // code 2
-        "", // code 3
-        "", // code 4
-        "" // code 5
-    ]
-    const [code, setCode] = useState(codeDefault)
-    const router = useRouter()
-    const codeRefs: any = []
-    // @ts-ignore
-    const refs_ = [...Array(5)].map((_, i) => {
-        codeRefs[i] = createRef()
-    })
-
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        codeRefs[0].current.focus()
-    }, [])
-
-    const clearForm = () => {
-        setCode(codeDefault)
-    }
-
-    // @ts-ignore
-    const onSubmit = async (e) => {
-        e.preventDefault()
-        let codeToSend = ""
-        code.forEach((item) => {
-            codeToSend += item
-        })
-        console.log(codeToSend)
-        const notification = await toast.loading('Laduje...')
+    const {register, handleSubmit, formState: {errors}, setError} = useForm();
+    const Alert = withReactContent(Swal)
+    const onSubmit = async (data: any) => {
         try {
-            const activateUserRes = await apiClient.auth.authActivateUser({
-                code: codeToSend
+            const response = await shark_api.auth.activateUser({
+                code: data.activation_code
             })
-            toast.update(notification, {
-                render: "Pomyślnie aktywowano konto",
-                type: "success",
-                isLoading: false,
-                autoClose: 4000,
+            await Alert.fire({
+                icon: 'success',
+                title: 'Konto zostało aktywowane',
+                showConfirmButton: false,
+                timer: 1500
             })
-        } catch (error: any) {
-            console.log(error)
-            toast.update(notification, {
-                // @ts-ignore
-                render: error.message === "Bad Request" ? "Nie poprawny kod" : error.message,
-                type: "error",
-                isLoading: false,
-                autoClose: 4000,
+
+        } catch (e) {
+            await Alert.fire({
+                icon: 'error',
+                title: 'Code is invalid or expired',
+                showConfirmButton: false,
+                timer: 1500
             })
         }
 
-        clearForm()
-    }
-
-    // @ts-ignore
-    const onChangeInput = (index: number, value) => {
-        let code_copy = [...code]
-        code_copy[index] = value
-        setCode(code_copy)
-
-        if (index >= 4) return
-
-        console.log(codeRefs[index])
-        codeRefs[index + 1].current.focus()
-        // @ts-ignore
     }
 
     return (
-        <form className="login-form" onSubmit={(e) => onSubmit(e)}>
+        <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
             <div className="row justify-content-center align-items-center text-center">
                 <div className="col-md-12">
-                    {error && (<AlertMessage message={error} show={true} type={"danger"}/>)}
-                </div>
-                {[...Array(5)].map((e, i) =>
-                    <div className="col-md-2 col-sm-3" key={i}>
-                        <div className="single-input-unit">
-                            <input
-                                ref={codeRefs[i]}
-                                id={`activate_code_${i + 1}`}
-                                className={"text-center"}
-                                value={code[i]}
-                                onChange={(e) => onChangeInput(i, e.target.value)}
-                                maxLength={1}
-                            />
-                        </div>
+                    <div className="single-input-unit">
+                        {errors?.activation_code?.type === "required" &&
+                            <span className="text-danger">Pole jest wymagane</span>}
+                        {errors?.activation_code?.type === "minLength" || errors?.activation_code?.type === "maxLength" &&
+                            <span className="text-danger">Pole musi zawierać 5 znaków</span>}
+                        <input type="text" id="activation_code" placeholder="Kod aktywacyjny"
+                               {...register("activation_code", {required: true, minLength: 5, maxLength: 5})}/>
                     </div>
-                )}
+                </div>
             </div>
 
             <div className="login-btn">
