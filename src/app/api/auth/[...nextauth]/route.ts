84aaaa8926ebe36f244dcdc1e03f1163e79f31fb
Adrian Ciołek <ciolek.adrian@protonmail.com>
2023-07-26 18:01:50 +0200

    Updated login form and page

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
 import {NextApiRequest, NextApiResponse} from "next";
-import apiClient from "@/lib/api";
 import jwt from "jsonwebtoken";
-
-const refreshToken = async (refresh_token: string) => {
-    const res = await fetch("http://localhost/v1/auth/token/refresh", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({refresh_token: refresh_token}),
-    });
-    const data = await res.json();
-    return data.access_token;
-}
+import {SharkServersClient as shark_api } from "sharkservers-sdk";
 // @ts-ignore
 export const authOptions: NextAuthOptions = {
     secret: process.env.AUTH_SECRET,
@@ -29,30 +17,14 @@ export const authOptions: NextAuthOptions = {
         },
         // @ts-ignore
         async authorize(credentials, req) {
-            const tokenData = await apiClient.auth.authLoginUser({
+            const tokenData = await shark_api.auth.loginUser({
                 // @ts-ignore
                 username: credentials?.username,
                 // @ts-ignore
                 password: credentials?.password
             })
-            apiClient.request.config.TOKEN = tokenData.access_token
-            const user_info = await apiClient.users.usersGetLoggedUser()
-
-            /*const token_res = await fetch("http://localhost/v1/auth/token", {
-                method: 'POST',
-                body: form,
-            })
-            const token = await token_res.json()
-
-            const user_info_res = await fetch("http://localhost/v1/users/me", {
-                method: 'GET',
-                headers: {'Authorization': 'Bearer ' + token.access_token}
-            })
-
-            const user_info = await user_info_res.json()
-            if (token_res.ok && token && user_info_res.ok && user_info) {
-                return {...token, ...user_info}
-            }*/
+            shark_api.request.config.TOKEN = tokenData.access_token
+            const user_info = await shark_api.users.getLoggedUser()
             if (tokenData && user_info) {
                 return {...tokenData, ...user_info}
             }
@@ -70,19 +42,17 @@ export const authOptions: NextAuthOptions = {
             if (user) {
                 return {...token, ...user}
             }
-            console.log(`Token ${JSON.stringify(token)} User ${user}`)
             const decoded = jwt.decode(token.access_token)
             //@ts-ignore
             const tokenExpire = decoded && decoded.exp && decoded.exp * 1000 < Date.now()
-            console.log(`Token expired ${tokenExpire}`)
             // @ts-ignore
-            console.log(`Token expire date ${decoded.exp}`)
+            console.log(`Token expire ${tokenExpire}`)
 
             if (!tokenExpire) {
                 return {...token, ...user}
             }
-            const refresh_access_token = await refreshToken(token.refresh_token)
-            return {...token, access_token: refresh_access_token, ...user}
+            const refresh_token = await shark_api.auth.getAccessTokenFromRefreshToken(token.refresh_token)
+            return {...token, access_token: refresh_token.access_token, ...user}
         },
         //@ts-ignore
         async session({session, token, user}) {
@@ -90,40 +60,6 @@ export const authOptions: NextAuthOptions = {
             session.user = token
             return session
         },
-
-        /*
-
-        const callbacks = {
-    jwt: async ({ token, user }) => {
-        if (user) {
-            // This will only be executed at login. Each next invocation will skip this part.
-            token.accessToken = user.data.accessToken;
-            token.accessTokenExpiry = user.data.accessTokenExpiry;
-            token.refreshToken = user.data.refreshToken;
-        }
-
-        // If accessTokenExpiry is 24 hours, we have to refresh token before 24 hours pass.
-        const shouldRefreshTime = Math.round((token.accessTokenExpiry - 60 * 60 * 1000) - Date.now());
-
-        // If the token is still valid, just return it.
-        if (shouldRefreshTime > 0) {
-            return Promise.resolve(token);
-        }
-
-        // If the call arrives after 23 hours have passed, we allow to refresh the token.
-        token = refreshAccessToken(token);
-        return Promise.resolve(token);
-    },
-    session: async ({ session, token }) => {
-        // Here we pass accessToken to the client to be used in authentication with your API
-        session.accessToken = token.accessToken;
-        session.accessTokenExpiry = token.accessTokenExpiry;
-        session.error = token.error;
-
-        return Promise.resolve(session);
-    },
-}
-         */
     },
     pages: {
         signIn: '/auth/login',
@@ -131,7 +67,7 @@ export const authOptions: NextAuthOptions = {
     events: {
         // @ts-ignore
         async signIn(message) {
-            console.log(message + " signed in")
+            console.log(message.token.username + " signed in")
         },
         // @ts-ignore
         async signOut(message) {
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,8 @@
-import LoginMain from "@/components/Auth/LoginMain";
-import TitleSection from "@/components/Layout/TitleSection";
-import {useSession} from "next-auth/react";
 import {redirect} from "next/navigation";
 import {getServerSession, Session} from "next-auth";
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import Section from "@/components/Elements/Section";
+import LoginForm from "@/components/Auth/LoginForm";
 
 const isUserLogged = async () => {
     const session: Session | null = await getServerSession(authOptions)
@@ -13,11 +12,24 @@ const isUserLogged = async () => {
 export default async function LoginPage() {
     const user_logged = await isUserLogged()
     if (user_logged) {
-        redirect("/forum")
+        redirect("/")
     }
     return (
         <>
-            <LoginMain />
+            <Section>
+                <div className="row justify-content-center">
+                    <div className="col-xxl-6 col-xl-7 col-lg-8">
+                        <div className="login-wrapper pos-rel mb-40 wow fadeInUp">
+                            <div className="login-inner">
+                                <div className="login-content">
+                                    <h4>Zaloguj się</h4>
+                                    <LoginForm />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Section>
         </>
     )
 }
