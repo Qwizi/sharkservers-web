9227f8fd7539a29be2dc5a8bd7a97136fade09bf
Adrian Ciołek <ciolek.adrian@protonmail.com>
2023-09-09 20:48:31 +0200

    Added last online card on home page

--- src/components/users/username.tsx
+++ src/components/users/username.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 type UsernameType = {
     id: number | undefined,
     color: string | undefined;
-    username: string | undefined; 
+    username: string | undefined;
+    className?: string | undefined;
 }
-export default function Username({id, color, username}: UsernameType) {
+export default function Username({id, color, username, className}: UsernameType) {
     return (
-        <Link key={id} href={`/profile/${id}-${username}`} style={{color: color}}>{username}</Link>
+        <Link className={className} key={id} href={`/profile/${id}-${username}`} style={{color: color}}>{username}</Link>
     )
 }
